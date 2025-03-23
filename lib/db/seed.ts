03fee00c38e4aaa4f41e61646edb9af67ea88151
Vitalii <co4ckowoi.v@gmail.com>
2025-03-23 18:06:48 +0200

    add reviews

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -4,32 +4,51 @@ import data from '@/lib/data'
 import { connectToDatabase } from '.'
 import Product from './models/product.model'
 import User from './models/user.model'
+import Review from './models/review.model'
 
+loadEnvConfig(cwd())
 
-
-
-loadEnvConfig(cwd()) 
-
-
- const main = async () => {
+const main = async () => {
   try {
-    const { products, users } = data 
-    await connectToDatabase(process.env.MONGODB_URI) 
+    const { products, users, reviews } = data
+    await connectToDatabase(process.env.MONGODB_URI)
 
-     await User.deleteMany()
+    await User.deleteMany()
     const createdUser = await User.insertMany(users)
 
-    await Product.deleteMany() 
-    const createdProducts = await Product.insertMany(products) 
+    await Product.deleteMany()
+    const createdProducts = await Product.insertMany(products)
 
+    await Review.deleteMany()
+    const rws = []
+    for (let i = 0; i < createdProducts.length; i++) {
+      let x = 0
+      const { ratingDistribution } = createdProducts[i]
+      for (let j = 0; j < ratingDistribution.length; j++) {
+        for (let k = 0; k < ratingDistribution[j].count; k++) {
+          x++
+          rws.push({
+            ...reviews.filter((x) => x.rating === j + 1)[
+              x % reviews.filter((x) => x.rating === j + 1).length
+            ],
+            isVerifiedPurchase: true,
+            product: createdProducts[i]._id,
+            user: createdUser[x % createdUser.length]._id,
+            updatedAt: Date.now(),
+            createdAt: Date.now(),
+          })
+        }
+      }
+    }
+    const createdReviews = await Review.insertMany(rws)
 
     console.log({
       createdUser,
       createdProducts,
+      createdReviews,
       message: 'Seeded database successfully',
     })
-    process.exit(0) 
-
+    process.exit(0)
   } catch (error) {
     console.error(error)
     throw new Error('Failed to seed database')
--- lib/validator.ts
+++ lib/validator.ts
@@ -13,6 +13,20 @@ const Price = (field: string) =>
       (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
       `${field} must have exactly two decimal places (e.g., 49.99)`
   )
+
+
+  export const ReviewInputSchema = z.object({
+  product: MongoId,
+  user: MongoId,
+  isVerifiedPurchase: z.boolean(),
+  title: z.string().min(1, 'Title is required'),
+  comment: z.string().min(1, 'Comment is required'),
+  rating: z.coerce
+    .number()
+    .int()
+    .min(1, 'Rating must be at least 1')
+    .max(5, 'Rating must be at most 5'),
+})
     
 
 
@@ -45,7 +59,7 @@ const Price = (field: string) =>
   ratingDistribution: z
     .array(z.object({ rating: z.number(), count: z.number() }))
       .max(5),
-  reviews: z.array (z.string()).default([]),
+  reviews: z.array(ReviewInputSchema).default([]),
   numSales: z.coerce
     .number()
     .int()
