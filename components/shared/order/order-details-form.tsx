195de59c9bf2cc410b10c1370951794ce0633f1d
Vitalii <co4ckowoi.v@gmail.com>
2025-03-31 18:46:05 +0300

    mark orders as paid delivered

--- components/shared/order/order-details-form.tsx
+++ components/shared/order/order-details-form.tsx
@@ -17,6 +17,8 @@ import { IOrder } from '@/lib/db/models/order.model'
 import { cn, formatDateTime } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import ProductPrice from '../product/product-price'
+import ActionButton from '../action-button'
+import { deliverOrder, updateOrderToPaid } from '@/lib/actions/order.actions'
 
 export default function OrderDetailsForm({
   order,
@@ -164,7 +166,16 @@ export default function OrderDetailsForm({
             )}
 
             {isAdmin && !isPaid && paymentMethod === 'Cash On Delivery' && (
-              <p></p>
+              <ActionButton
+                caption="Mark as paid"
+                action={() => updateOrderToPaid(order._id)}
+              />
+            )}
+            {isAdmin && isPaid && !isDelivered && (
+              <ActionButton
+                caption="Mark as delivered"
+                action={() => deliverOrder(order._id)}
+              />
             )}
           </CardContent>
         </Card>
